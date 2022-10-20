53b22f747234116e337778a347510f72f712741a
Andreen-Muchiri <andreenshii@gmail.com>
2022-10-20 14:55:39 +0300

    Complete assignment

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+# create an object instance using class
+# inside object instance create a method instance
+class Dog
+  
+    def bark
+    puts "Woof!"
+    end
+    
+    def sit
+    puts "The Dog is sitting"
+    end
+
+end
